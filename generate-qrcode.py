1478a45494dc0e5a0d0168b409759452b13e4677
miyaryo <83214456+miyaryo1212@users.noreply.github.com>
2023-01-13 17:01:54 +0900

    Update generate-qrcode.py: gui features

--- generate-qrcode.py
+++ generate-qrcode.py
@@ -1,5 +1,31 @@
 import ctypes
 
+import tkinter
+import tkinter.filedialog
+import tkinter.messagebox
+
+
+# gui messagebox
+def msgbox(title, content):
+    root = tkinter.Tk()
+    root.withdraw()
+    tkinter.messagebox.showinfo(title, content)
+
+    return None
+
+
+# gui folderdialog
+def openfolder():
+    root = tkinter.Tk()
+    root.withdraw()
+    dir_path = tkinter.filedialog.askdirectory()
+
+    if not dir_path:
+        quit()
+
+    return dir_path
+
+
 if __name__ == "__main__":
     try:
         ctypes.windll.shcore.SetProcessDpiAwareness(True)
