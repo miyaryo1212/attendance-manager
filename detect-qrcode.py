e2dcbe78871d6d7489c3132722d431b0612528c8
miyaryo <83214456+miyaryo1212@users.noreply.github.com>
2023-01-13 17:59:12 +0900

    Update detect-qrcode.py: gui features

--- detect-qrcode.py
+++ detect-qrcode.py
@@ -1,4 +1,32 @@
 import ctypes
+import tkinter
+import tkinter.filedialog
+import tkinter.messagebox
+
+import cv2 as cv
+import pyzbar
+
+
+# gui messagebox
+def msgbox(title, content):
+    root = tkinter.Tk()
+    root.withdraw()
+    tkinter.messagebox.showinfo(title, content)
+
+    return None
+
+
+# gui filedialog
+def openfile(file_type=[("", "*")]):
+    root = tkinter.Tk()
+    root.withdraw()
+    file_path = tkinter.filedialog.askopenfilename(filetypes=file_type)
+
+    if not file_path:
+        quit()
+
+    return file_path
+
 
 if __name__ == "__main__":
     try:
