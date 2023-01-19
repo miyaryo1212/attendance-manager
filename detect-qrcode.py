4a691e34361d4493dbdaa8c83ab679ccfd775ee2
miyaryo <83214456+miyaryo1212@users.noreply.github.com>
2023-01-19 16:12:18 +0900

    Update detect-qrcode.py: lists, generate worksheet

--- detect-qrcode.py
+++ detect-qrcode.py
@@ -1,10 +1,13 @@
 import copy
 import ctypes
+import datetime
+import os
 import time
 import tkinter
 import tkinter.filedialog
 import tkinter.messagebox
 
+import openpyxl
 import cv2 as cv
 import numpy as np
 import pyautogui
@@ -20,104 +23,48 @@ color = {
 # optimized for my shcool
 # 11-19HR, 21-29HR, 30-39HR each class has less than 45 students
 # inactive=-1 / active=1
-status = {
-    "11": np.full(45, -1),
-    "11": np.full(45, -1),
-    "11": np.full(45, -1),
-    "12": np.full(45, -1),
-    "13": np.full(45, -1),
-    "14": np.full(45, -1),
-    "15": np.full(45, -1),
-    "16": np.full(45, -1),
-    "17": np.full(45, -1),
-    "18": np.full(45, -1),
-    "19": np.full(45, -1),
-    "21": np.full(45, -1),
-    "22": np.full(45, -1),
-    "23": np.full(45, -1),
-    "24": np.full(45, -1),
-    "25": np.full(45, -1),
-    "26": np.full(45, -1),
-    "27": np.full(45, -1),
-    "28": np.full(45, -1),
-    "29": np.full(45, -1),
-    "30": np.full(45, -1),
-    "31": np.full(45, -1),
-    "32": np.full(45, -1),
-    "33": np.full(45, -1),
-    "34": np.full(45, -1),
-    "35": np.full(45, -1),
-    "36": np.full(45, -1),
-    "37": np.full(45, -1),
-    "38": np.full(45, -1),
-    "39": np.full(45, -1),
-}
+HRs = [
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+    "28",
+    "29",
+    "30",
+    "31",
+    "32",
+    "33",
+    "34",
+    "35",
+    "36",
+    "37",
+    "38",
+    "39",
+]
 
-last_updated_time = {
-    "11": np.full(45, time.time()),
-    "11": np.full(45, time.time()),
-    "11": np.full(45, time.time()),
-    "12": np.full(45, time.time()),
-    "13": np.full(45, time.time()),
-    "14": np.full(45, time.time()),
-    "15": np.full(45, time.time()),
-    "16": np.full(45, time.time()),
-    "17": np.full(45, time.time()),
-    "18": np.full(45, time.time()),
-    "19": np.full(45, time.time()),
-    "21": np.full(45, time.time()),
-    "22": np.full(45, time.time()),
-    "23": np.full(45, time.time()),
-    "24": np.full(45, time.time()),
-    "25": np.full(45, time.time()),
-    "26": np.full(45, time.time()),
-    "27": np.full(45, time.time()),
-    "28": np.full(45, time.time()),
-    "29": np.full(45, time.time()),
-    "30": np.full(45, time.time()),
-    "31": np.full(45, time.time()),
-    "32": np.full(45, time.time()),
-    "33": np.full(45, time.time()),
-    "34": np.full(45, time.time()),
-    "35": np.full(45, time.time()),
-    "36": np.full(45, time.time()),
-    "37": np.full(45, time.time()),
-    "38": np.full(45, time.time()),
-    "39": np.full(45, time.time()),
-}
+status = {}
+for HR in HRs:
+    status[HR] = np.full(45, -1)
 
-active_members = {
-    "11": [],
-    "11": [],
-    "11": [],
-    "12": [],
-    "13": [],
-    "14": [],
-    "15": [],
-    "16": [],
-    "17": [],
-    "18": [],
-    "19": [],
-    "21": [],
-    "22": [],
-    "23": [],
-    "24": [],
-    "25": [],
-    "26": [],
-    "27": [],
-    "28": [],
-    "29": [],
-    "30": [],
-    "31": [],
-    "32": [],
-    "33": [],
-    "34": [],
-    "35": [],
-    "36": [],
-    "37": [],
-    "38": [],
-    "39": [],
-}
+last_updated_time = {}
+for HR in HRs:
+    status[HR] = np.full(45, time.time())
+
+active_members = {}
+for HR in HRs:
+    active_members[HR] = []
 
 last_log = time.time()
 
@@ -306,4 +253,21 @@ if __name__ == "__main__":
     except:
         pass
 
-    readvideo(0)
+    dirpath = "./output"
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+    else:
+        pass
+
+    bookpath = "/{}.xlsx".format(datetime.datetime.now().strftime("%Y-%m-%d"))
+    if not os.path.exists(dirpath + bookpath):
+        openpyxl.Workbook().save(dirpath + bookpath)
+        workbook = openpyxl.load_workbook(dirpath + bookpath)
+        for HR in HRs:
+            workbook.create_sheet("{}HR".format(HR))
+        workbook.save(dirpath + bookpath)
+        print("Created workbook")
+    else:
+        pass
+
+    # readvideo(0)
